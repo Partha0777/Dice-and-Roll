ac0ebf0492cad4e9fb12b14cf3ea55bc36acfae2
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-01-07 19:20:37 +0530

    #Creates separate constants file for add all constants values.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
+
 const appName = "Dice And Roll!";
 const splashDuration = 2;
+
 const image = "assets/images/dice_{}.png";
 const imagePlaceHolderValue = "{}";
 const buttonDiceAndRoll = "Click to Dice And Roll!!";
