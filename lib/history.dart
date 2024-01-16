f802f9132c0d23f21a59f6da0a6de6c4f32c0304
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-01-16 22:02:58 +0530

    #Created history view for show the last history of the dice and roll. need to add UI..

---
+++ lib/history.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class History extends StatefulWidget {
+  const History({super.key});
+
+  @override
+  State<StatefulWidget> createState() => HistoryState();
+}
+
+class HistoryState extends State<History> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(),
+    );
+  }
+}
