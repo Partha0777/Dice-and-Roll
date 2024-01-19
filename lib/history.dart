cd32e734e60d2b38b8c924ce0161aa9bb5e5d239
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-01-19 23:02:21 +0530

    #Added constructor for retrieve the history data from the home.

--- lib/history.dart
+++ lib/history.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class History extends StatefulWidget {
-  const History({super.key});
+  const History({super.key, required this.historyList});
+
+  final List<int> historyList;
 
   @override
   State<StatefulWidget> createState() => HistoryState();
